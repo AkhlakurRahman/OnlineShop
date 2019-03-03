b66b39c8177a3ed8c40bd587cfc824f713209fc6
AkhlakurRahman <akhlakur.rahmann@gmail.com>
2019-03-04 01:31:11 +0600

    online shop project completed

---
+++ routes/auth.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const { check, body } = require('express-validator/check');
+
+const authController = require('../controllers/auth');
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.get('/login', authController.getLogin);
+
+router.get('/signup', authController.getSignup);
+
+router.post(
+	'/login',
+	[
+		body('email')
+			.isEmail()
+			.withMessage('Please enter a valid email address.')
+			.normalizeEmail(),
+		body('password', 'Password has to be valid.')
+			.isLength({ min: 4 })
+			.trim()
+	],
+	authController.postLogin);
+
+router.post(
+	'/signup',
+	[
+	check('email')
+		.isEmail()
+		.withMessage('Please enter a valid email.')
+		.custom((value, { req }) => {
+			return User.findOne({ email: value })
+				.then(userDoc => {
+					if (userDoc) {
+						return Promise.reject('Email already exists, try with another!');
+					}
+
+				});
+		})
+		.normalizeEmail(),
+
+	body(
+		'password',
+		'Password must be at least 4 characters long.'
+		)
+		.isLength({ min: 4 })
+		.trim(),
+
+	body('confirmPassword')
+		.custom((value, { req }) => {
+			if (value !== req.body.password) {
+				throw new Error ('Password doesn\'t match!')
+			}
+			return true;
+		})
+		.trim()
+	],
+	authController.postSignup);
+
+router.post('/logout', authController.postLogout);
+
+router.get('/reset', authController.getReset);
+
+router.post('/reset', authController.postReset);
+
+router.get('/reset/:token', authController.getNewPassword);
+
+router.post('/new-password', authController.postNewPassword);
+
+module.exports = router;
